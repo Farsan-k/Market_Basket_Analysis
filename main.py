8266ee914c79ee8770c1efd65d26f73650562ba0
Farsan <farsanfarsank41@gmail.com>
2026-08-11 22:03:58 +0530

    Deployment fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
     imputer = SizeImputer(column='product_size_clean')
     df = imputer.fit_transform(df)
 
-    df.to_csv("final.csv", index=False)
+    df.to_csv("data/final.csv", index=False)
 
     category_rules = category_model(df)
     print(category_rules.head(10))
@@ -28,7 +28,7 @@ def main():
     plot_top_rules(category_rules, save_path="output/category_plot")
     plot_top_rules(pasta_rules, save_path="output/pasta_plot")
 
-    save_rules(category_rules, "rules.csv")
+    save_rules(category_rules, "data/rules.csv")
 
 
 if __name__ == "__main__":
